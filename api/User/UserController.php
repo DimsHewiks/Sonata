c5c962789cea5b199d766bee742dcfb5931ad16c
DimsHewik <DimsHewik@mail.ru>
2026-01-24 20:02:47 +0800

    добавлен свагер

--- api/User/UserController.php
+++ api/User/UserController.php
@@ -3,26 +3,15 @@
 namespace Api\User;
 
 use Api\User\Dto\Response\UserResponse;
-use Api\User\Dto\UserCreateDto;
 use Core\Attributes\Controller;
 use Core\Attributes\Response;
 use Core\Attributes\Route;
-use Core\Attributes\From;
 use Core\Attributes\Tag;
-use OpenApi\Tests\Fixtures\Parser\User;
 
 #[Controller(prefix: '/api')]
 #[Tag('Пользователи', 'Регистрация пользователя')]
 class UserController
 {
-    /**
-     * @return UserResponse[]
-     */
-    /**
-     * Получить список пользователей
-     *
-     * @return UserResponse[]
-     */
     #[Route(path: '/users', method: 'GET')]
     #[Response(UserResponse::class, isArray: true)]
     public function listUsers(): array
