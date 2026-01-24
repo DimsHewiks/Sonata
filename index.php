8c6a20f75871ea9341aade5d2ed8510506408c84
DimsHewik <DimsHewik@mail.ru>
2026-01-24 16:11:30 +0800

    рефактор индекса

--- index.php
+++ index.php
@@ -1,29 +1,23 @@
 <?php
+
 declare(strict_types=1);
 
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
-error_log("Base URL: " . $_SERVER['REQUEST_URI']);
-error_log("SCRIPT_NAME: " . $_SERVER['SCRIPT_NAME']);
-
-require __DIR__.'/vendor/autoload.php';
-require_once __DIR__ . '/bootstrap.php';
-
-$envPath = __DIR__.'/';
-try {
-    $dotenv = Dotenv\Dotenv::createImmutable($envPath);
-    $dotenv->safeLoad();
-} catch (Dotenv\Exception\InvalidPathException $e) {
-    die('Please create .env file. Copy from .env.example');
+if (getenv('APP_ENV') === 'dev') {
+    error_log("Base URL: " . ($_SERVER['REQUEST_URI'] ?? ''));
+    error_log("SCRIPT_NAME: " . ($_SERVER['SCRIPT_NAME'] ?? ''));
 }
 
-$debug = getenv('APP_ENV') === 'dev';
 $container = require __DIR__ . '/bootstrap.php';
+
+$debug = getenv('APP_ENV') === 'dev';
+
 $router = new Core\Router($container, $debug);
 $router->registerControllers();
 
 $router->dispatch(
-    parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH),
+    parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH) ?: '/',
     $_SERVER['REQUEST_METHOD']
 );
