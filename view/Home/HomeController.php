965fa8e645a2b578fa61b4b6a2a51dba180c27ff
DimsHewik <DimsHewik@mail.ru>
2026-01-25 21:29:04 +0800

    fix html

--- view/Home/HomeController.php
+++ view/Home/HomeController.php
@@ -3,22 +3,26 @@ namespace View\Home;
 
 use Core\Attributes\Controller;
 use Core\Attributes\Route;
+use Core\Attributes\Tag;
+use Core\Http\Response;
 use View\Home\Pages\HomePage;
 
 
 #[Controller(prefix: '/')]
+#[Tag('Статика')]
 class HomeController
 {
-    #[Route(path: '/', method: 'GET')]
-    public function test(): string
+    #[Route(path: '/', method: 'GET', summary: 'Главная страница')]
+    public function home(): never
     {
-        return new HomePage()->home();
+        $html = new HomePage()->home();
+        Response::html($html, 200);
     }
 
-    #[Route(path: '/about', method: 'GET')]
-    public function about():string
+    #[Route(path: '/about', method: 'GET', summary: 'тестовая страница')]
+    public function about(): never
     {
-        return '<h2>УВА Молодец!!!</h2>';
+        Response::html('<h2>УВА Молодец!!!</h2>', 200);
     }
 
 
